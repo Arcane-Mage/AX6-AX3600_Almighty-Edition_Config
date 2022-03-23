1bb5841fbd0e22ae0b3538341dc06c9ecc1ff70c
Extreme <extreme_hust@outlook.com>
2022-03-24 00:39:10 +0800

    Fix: simplify passwall2 plugins

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,7 +33,6 @@ git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
 git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-
 #添加smartdns
 git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
 
